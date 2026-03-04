3ae4c74162877a1634035dc7819c2c943a37d90d
andrearangel45 <124049331@upq.edu.mx>
2026-03-03 21:40:15 -0600

    peticiones al backend

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,12 @@ const API_URL = "http://localhost:4000/api"; // Importante colocar la url de tu 
 
 export const api = {
   get: async (endpoint) => {
+    const token = localStorage.getItem('token');
     try {
-      const response = await fetch(`${API_URL}${endpoint}`);
+      const response = await fetch(`${API_URL}${endpoint}`, {
+        headers: { 'Authorization': `Bearer ${token}` }
+      });
+
       if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
       return await response.json();
     } catch (error) {
@@ -13,10 +17,11 @@ export const api = {
   },
 
   post: async (endpoint, body) => {
+    const token = localStorage.getItem('token');
     try {
       const response = await fetch(`${API_URL}${endpoint}`, {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Authorization': token? `Bearer ${token}`: '', 'Content-Type': 'application/json' },
         body: JSON.stringify(body)
       });
       if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
